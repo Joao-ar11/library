5abdeffaffa496fd843af980669688b8d4d36efa
joao-ar11 <jarlpereira29@gmail.com>
2023-04-12 16:17:22 -0300

    Add addEntry to javascript.js

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ class Book {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.haveRead = haveRead === "on";
+    this.haveRead = haveRead;
   }
 }
 
@@ -17,12 +17,52 @@ function openModal() {
     });
 }
 
-function addBook(bookList) {
+function addToBookList(bookList) {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read").value;
+  const read = document.querySelector("#read").checked;
   bookList.push(new Book(title, author, pages, read));
+}
+
+function addEntry(book, id) {
+  const newBook = document.createElement("div");
+  newBook.classList.add("book");
+
+  const title = document.createElement("h2");
+  title.textContent = book.title;
+  newBook.appendChild(title);
+
+  const author = document.createElement("p");
+  author.textContent = `By: ${book.author}`;
+  newBook.appendChild(author);
+
+  const pages = document.createElement("p");
+  pages.textContent = book.pages;
+  newBook.appendChild(pages);
+
+  const checkDiv = document.createElement("div");
+  const check = document.createElement("input");
+  check.type = "checkbox";
+  check.id = `read-book-${id}`;
+  check.checked = book.haveRead;
+  const label = document.createElement("label");
+  label.textContent = "Have read";
+  label.htmlFor = `read-book-${id}`;
+  checkDiv.appendChild(check);
+  checkDiv.appendChild(label);
+  newBook.appendChild(checkDiv);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  newBook.appendChild(removeButton);
+
+  document.querySelector("main").appendChild(newBook);
+}
+
+function addBook(bookList) {
+  addToBookList(bookList);
+  addEntry(bookList[bookList.length - 1], bookList.length - 1);
 }
 
 const bookList = [];
